1944b9fa8876b0fd7c216734f924092d5721f60d
hemang003 <hemangparmar003@gmail.com>
2025-03-19 19:34:19 -0400

    First Commit (Initial)

---
+++ frontend/src/components/Here.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div className="hero flex items-center justify-between px-[100px] bg-red-500" style={{height: "calc(100vh - 100px)"}}>
+        <div className="left"></div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
