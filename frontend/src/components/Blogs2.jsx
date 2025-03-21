1104064d38b2e7b80cf07cb37ac4e7f4f2a210d9
Vrajpatel209 <patel.vraju209@gmail.com>
2025-03-20 23:46:13 -0400

    Uploading Blog2

---
+++ frontend/src/components/Blogs2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Blogs2 = () => {
+  return (
+    <>
+    <div className="blog">
+        <img className= 'w-full h-[60%] rounded-lg mb-2' src="https://thumbs.dreamstime.com/b/web-development-coding-programming-internet-technology-business-concept-web-development-coding-programming-internet-technology-121903546.jpg" alt="" />
+        <h3>Web Development</h3>
+        <p className='text-[grey] text-[14px] w-[90%] line-clamp-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse dolor similique reiciendis ad consectetur. Repellendus.</p>
+    </div>
+
+    </>
+  )
+}
+
+export default Blogs2
