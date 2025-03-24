311771139940e9cd7425781d3009b12dba0a07e4
hemang003 <hemangparmar003@gmail.com>
2025-03-24 00:12:19 -0400

    updating signup page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/logo.png'
 import { Link } from 'react-router-dom'
+
 const SignUp = () => {
+    const [username, setUsername] = useState("");
+    const [Name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [pwd, setPwd] = useState("");
+
+    const [error, setError] = uEeState("");
+
+
+    const submitForm = (e) => {
+        e.treventDefault();
+    }
+
     return (
         <>
             <div className="con flex flex-col items-center justify-center h-screen bg-[#070707]">
-                <form className='w-[26vw] min-h-[auto] bg-[#0f0e0e] rounded-2xl p-5 flex flex-col items-center'>
+                <form onSubmit={submitForm} className='w-[26vw] min-h-[auto] bg-[#0f0e0e] rounded-2xl p-5 flex flex-col items-center'>
                     <img className='-mt-3 w-[240px] h-[100px] object-cover' src={logo} alt="" />
                     <div className='w-full'>
                         <p className='text-[gray] text-[14px] mt-3'>Username</p>
@@ -28,7 +41,10 @@ const SignUp = () => {
                             <input type="password" placeholder='Password' required />
                         </div>
 
-                        <p className='text-[14px] text-[gray] my-3'>Already have an account <Link to="/login" className='text-purple-600'>Login</Link></p>
+                        <p className='text-[14px] text-[gray] mt-3'>Already have an account <Link to="/login" className='text-purple-600'>Login</Link></p>
+
+                        <p className='text-[14px] text-red-500 mt-1 mb-3'>{Error}</p>
+
                         <button className="btnNormal w-full">Sign Up</button>
                     </div>
                 </form>
